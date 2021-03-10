6d052e3b03ada128763134a1993e00e386efcb5c
Thangamanir <thangamani.r@gmail.com>
2021-03-10 23:40:32 +0800

    Added firebase files

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,14 @@ import firebase from 'firebase';
 
 const firebaseConfig={
     //Fill in your firebase details here
-    
+    apiKey: "AIzaSyDR5ZGNKSbpZ71uXOtH99-tX5qSWzgBr9M",
+    authDomain: "bt3103-2020.firebaseapp.com",
+    databaseURL: "https://bt3103-2020.firebaseio.com",
+    projectId: "bt3103-2020",
+    storageBucket: "bt3103-2020.appspot.com",
+    messagingSenderId: "587840778474",
+    appId: "1:587840778474:web:e5cd103f5580b10d7de793",
+    measurementId: "G-XW88WQ8LRR"
 }
 
 // Initialize Firebase
